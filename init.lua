4395106d6d4695214bf091db1b62858172e5afe6
TenEXP <10exp@protonmail.com>
2023-06-06 18:47:56 +0500

    cleanup, horizontal term keybind

--- init.lua
+++ init.lua
@@ -2,3 +2,10 @@ require "options"
 require "keymaps"
 require "Lazy"
 require "autocommands"
+
+-- Neovide
+if vim.g.neovide then
+  vim.o.guifont = "JetBrainsMono Nerd Font:h11"
+  vim.g.neovide_scale_factor = 1
+  vim.g.neovide_cursor_vfx_mode = "sonicboom"
+end
