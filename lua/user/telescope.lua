63b5b8f2f22724e172f6991186b3fc38871bddfa
TenEXP <10exp@protonmail.com>
2023-06-07 14:47:49 +0500

    Set telescope to search hidden directories/files

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -30,4 +30,26 @@ M.opts = {
   },
 }
 
+local telescope = require("telescope")
+local telescopeConfig = require("telescope.config")
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+-- Search hidden directories and files
+table.insert(vimgrep_arguments, "--hidden")
+-- Don't look into .git
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
+telescope.setup({
+	defaults = {
+		-- `hidden = true` is not supported in text grep commands.
+		vimgrep_arguments = vimgrep_arguments,
+	},
+	pickers = {
+		find_files = {
+			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+		},
+	},
+})
+
 return M
